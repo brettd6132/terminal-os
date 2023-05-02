86540dae2995b79036de339ecb28b826e07b117b
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2023-05-02 03:00:54 -0700

    Delete storeLicenseKey.kt

--- license/storeLicenseKey.kt
+++
@@ -1,8 +0,0 @@
-import java.io.File
-import java.util.UUID
-
-fun storeLicenseKey(licenseKey: String) {
-    val filename = "${UUID.randomUUID()}.txt"
-    val file = File("licenses/$filename")
-    file.writeText(licenseKey)
-}
