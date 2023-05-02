24721ab0205486b44f9d2b11035e55ca65d474b6
Bolt Devs <82177322+daboltgod@users.noreply.github.com>
2023-05-02 03:29:11 -0700

    Delete License.kt

--- src/main/kotlin/license/License.kt
+++
@@ -1,9 +0,0 @@
-import java.time.LocalDateTime
-
-data class License(
-    val key: String,
-    val userIdentifier: String,
-    val command: String,
-    val issuedAt: LocalDateTime,
-    var used: Boolean = false
-)
